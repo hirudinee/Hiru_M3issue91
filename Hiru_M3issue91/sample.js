36226388322d3d42c7e235bae2a438837c734919
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-29 17:51:13 +0530

    second page

--- Hiru_M3issue91/sample.js
+++ Hiru_M3issue91/sample.js
@@ -13,11 +13,10 @@ exports.handler = function (event, context, callback) {
 		WaitTimeSeconds: '0'
 	}, function (receivedMessages) {
 		receivedMessages.forEach(message => {
-			// your logic to access each message through out the loop. Each message is available under variable message 
-			// within this block
+			console.log(message);
 		})
 	}, function (error) {
-		// implement error handling logic here
+		console.log(error);
 	});
 
 	sqs.sendMessage({
@@ -26,9 +25,9 @@ exports.handler = function (event, context, callback) {
 		DelaySeconds: '0',
 		MessageAttributes: {}
 	}, function (data) {
-		// your logic (logging etc) to handle successful message delivery, should be here
+		console.log(data);
 	}, function (error) {
-		// your logic (logging etc) to handle failures, should be here
+		console.log(error);
 	});
 
 
