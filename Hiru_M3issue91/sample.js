effc4ad81c7ef0a051187d2c125efa738746548a
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-29 17:46:57 +0530

    second page

--- Hiru_M3issue91/sample.js
+++ Hiru_M3issue91/sample.js
@@ -1,23 +1,35 @@
 let AWS = require('aws-sdk');
+let SL = require('@slappforge/slappforge-sdk');
+const sqs = new SL.AWS.SQS(AWS);
 const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
 
 
-
-	s3.getBucketLocation({
-		'Bucket': "hiru.sample"
-	}).promise()
-		.then(data => {
-			console.log(data);           // successful response
-			/*
-			data = {
-				LocationConstraint: "us-west-2"
-			}
-			*/
+	sqs.receiveMessage({
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/Hiru_Test1201',
+		AttributeNames: ['All'],
+		MaxNumberOfMessages: '1',
+		VisibilityTimeout: '30',
+		WaitTimeSeconds: '0'
+	}, function (receivedMessages) {
+		receivedMessages.forEach(message => {
+			// your logic to access each message through out the loop. Each message is available under variable message 
+			// within this block
 		})
-		.catch(err => {
-			console.log(err, err.stack); // an error occurred
-		});
+	}, function (error) {
+		// implement error handling logic here
+	});
+
+	sqs.sendMessage({
+		MessageBody: 'this is a sample message',
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/Hiru_Test1201',
+		DelaySeconds: '0',
+		MessageAttributes: {}
+	}, function (data) {
+		// your logic (logging etc) to handle successful message delivery, should be here
+	}, function (error) {
+		// your logic (logging etc) to handle failures, should be here
+	});
 
 
 	callback(null, 'Successfully executed');
