51bee47d5ec7ac98dea5413c9bef97884fe5aa52
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-29 15:59:48 +0530

    s3 trigger

--- Hiru_M3issue91/lambda.js
+++ Hiru_M3issue91/lambda.js
@@ -1,26 +1,27 @@
 let AWS = require('aws-sdk');
+let SL = require('@slappforge/slappforge-sdk');
+const sqs = new SL.AWS.SQS(AWS);
 const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
 
-	s3.putObject({
-		"Body": "test",
-		"Bucket": "hiru.sample",
-		"Key": "test"
-	})
-		.promise()
-		.then(data => {
-			console.log(data);           // successful response
-			/*
-			data = {
-				ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
-				VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-			}
-			*/
-		})
-		.catch(err => {
-			console.log(err, err.stack); // an error occurred
-		});
+    sqs.receiveMessage({
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/Hiru_Test1201',
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
+    }, function (receivedMessages) {
+        receivedMessages.forEach(message => {
 
+            console.log('message ', message);
+            // your logic to access each message through out the loop. Each message is available under variable message 
+            // within this block
+        })
+    }, function (error) {
+        console.log('error ', error);
+        // implement error handling logic here
+    });
 
-	callback(null, 'Successfully executed');
+    callback(null, 'Successfully executed');
+
 }
