fc61a24f8371a5019e467b8ea881d1d3a1a71471
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-29 17:28:12 +0530

    second page

--- Hiru_M3issue91/sample.js
+++ Hiru_M3issue91/sample.js
@@ -1,11 +1,11 @@
 let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
-	
-	
-	
+
+
+
 	s3.getBucketLocation({
-		'Bucket': "hiru.delete"
+		'Bucket': "hiru.sample"
 	}).promise()
 		.then(data => {
 			console.log(data);           // successful response
